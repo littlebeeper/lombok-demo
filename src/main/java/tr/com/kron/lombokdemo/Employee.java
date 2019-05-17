43a905f2350b3f294c906b24c0a3ef9cf0ebd512
Cuneyt Ozen <cuneyt.ozen@kron.com.tr>
2019-05-17 05:38:44 +0300

    Constructs demo skeleton

---
+++ src/main/java/tr/com/kron/lombokdemo/Employee.java
@@ -0,0 +1,57 @@
+package tr.com.kron.lombokdemo;
+
+import java.util.Objects;
+
+public class Employee {
+    private String firstName;
+    private String lastname;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return firstName.equals(employee.firstName) &&
+                lastname.equals(employee.lastname) &&
+                email.equals(employee.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastname, email);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
